ecf8eacb880edbe205be8292be32a076bb6a63e6
alfred0105 <ace060408@gmail.com>
2025-12-27 02:41:27 +0900

    Ensure initProject is called on startup to handle URL parameters and project loading

--- js/modules/Main.js
+++ js/modules/Main.js
@@ -1,9 +1,12 @@
-/**
+﻿/**
  * 메인 엔트리 포인트
  */
 document.addEventListener('DOMContentLoaded', () => {
     // 시뮬레이터 인스턴스 생성
     window.sim = new CircuitSimulator();
+    if (window.sim.initProject) {
+        window.sim.initProject();
+    }
 
     // 탭 시스템 초기화
     if (window.sim.initTabs) {
